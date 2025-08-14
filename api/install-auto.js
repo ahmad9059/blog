fffbbc00db32700de0e5200fb346c9b90b4f6b17
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-14 21:29:18 +0500

    a

--- api/install-auto.js
+++ api/install-auto.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const url =
-    "https://raw.githubusercontent.com/ahmad9059/Scripts/main/boot.sh";
+    "https://raw.githubusercontent.com/ahmad9059/Scripts/main/boot-auto.sh";
   const response = await fetch(url);
 
   if (!response.ok) {
